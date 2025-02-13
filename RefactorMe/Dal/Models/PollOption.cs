d04cd234c825754ee3fe35719e2695d146184589
Bery0za <bereznevtree@yandex.ru>
2025-02-13 11:11:06 +0500

    Consistent indentation

--- RefactorMe/Dal/Models/PollOption.cs
+++ RefactorMe/Dal/Models/PollOption.cs
@@ -2,7 +2,7 @@
 
 public class PollOption
 {
-	public int Id { get; set; }
-	public string Text { get; set; }
-	public int QuestionId { get; set; }
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public int QuestionId { get; set; }
 }
--- RefactorMe/Dto/UserStatsDto.cs
+++ RefactorMe/Dto/UserStatsDto.cs
@@ -2,9 +2,9 @@
 
 public class UserStatsDto
 {
-	public string Name { get; set; }
-	public int Last30DaysSurveysCount { get; set; }
-	public int Last30DaysScore { get; set; }
-	public int TotalSurveysCount { get; set; }
-	public int TotalScore { get; set; }
+    public string Name { get; set; }
+    public int Last30DaysSurveysCount { get; set; }
+    public int Last30DaysScore { get; set; }
+    public int TotalSurveysCount { get; set; }
+    public int TotalScore { get; set; }
 }
